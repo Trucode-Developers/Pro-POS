249c6124f4e21938adec05767abe7d36f5b12a7c
silomalo <silomalojoseph@gmail.com>
2024-03-12 02:20:13 +0300

    sqlite and postgress running together

--- src-tauri/src/routes/users copy.rs
+++ src-tauri/src/routes/users copy.rs
@@ -1,21 +1,16 @@
+use serde::{Deserialize, Serialize};
+use sqlx::FromRow;
+use sqlx::PgPool;
+use tauri::State;
+use thiserror::Error;
 
-#[tauri::command]
-pub fn greet(name: &str) -> String {
-   format!("Hello, {}!", name)
+
+pub struct PgPoolWrapper {
+    pub pool: PgPool,
 }
 
-
-
-use std::error::Error;
-use std::sync::Arc;
-use tauri::State;
-use sqlx::PgPool;
-use serde::Deserialize;
-use serde::Serialize;
-
-use crate::AppState;
-
-#[derive(Debug, Deserialize)]
+// #[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, FromRow)]
 pub struct User {
     pub name: String,
     pub role: i32,
@@ -24,47 +19,106 @@ pub struct User {
     pub is_active: bool,
 }
 
-#[derive(Serialize)]
-pub struct CommandResult {
-    success: bool,
-    // You can add more fields if needed
+
+
+#[derive(Error, Serialize, Debug)]
+pub enum CustomError {
+    #[error("An error occurred")]
+    GenericError,
+    // Add other error variants as needed
 }
 
-// Create a new user
 #[tauri::command]
-pub async fn create(user: User, state: State<'_, Arc<AppState>>) -> Result<CommandResult, Box<dyn Error>> {
-    let query = "INSERT INTO users (name, role, email, password, is_active) VALUES ($1, $2, $3, $4, $5)";
+pub async fn greet(name: String) -> String {
+    format!("Hello, {}!", name)
+}
 
-    // Bind user data to the SQL query and execute it
-    sqlx::query(query)
+#[tauri::command]
+pub async fn create(user: User, state: State<'_, PgPoolWrapper>) -> Result<(), CustomError> {
+    let query =
+        "INSERT INTO users (name, role, email, password, is_active) VALUES ($1, $2, $3, $4, $5)";
+
+    let result = sqlx::query(query)
         .bind(&user.name)
         .bind(&user.role)
         .bind(&user.email)
         .bind(&user.password)
         .bind(&user.is_active)
-        .execute(&*state.db_pool)
-        .await?;
+        .execute(&state.pool)
+        .await;
 
-    // Return a success result
-    Ok(CommandResult { success: true })
+    match result {
+        Ok(_) => Ok(()),
+        Err(_) => Err(CustomError::GenericError),
+    }
 }
 
-// Update an existing user by ID
 #[tauri::command]
-pub async fn update(id: i32, user: User, state: State<'_, Arc<AppState>>) -> Result<CommandResult, Box<dyn Error>> {
+pub async fn update(
+    id: i32,
+    user: User,
+    state: State<'_, PgPoolWrapper>,
+) -> Result<(), CustomError> {
     let query = "UPDATE users SET name = $1, role = $2, email = $3, password = $4, is_active = $5 WHERE id = $6";
 
-    // Bind user data and ID to the SQL query and execute it
-    sqlx::query(query)
+    let result = sqlx::query(query)
         .bind(&user.name)
         .bind(&user.role)
         .bind(&user.email)
         .bind(&user.password)
         .bind(&user.is_active)
         .bind(&id)
-        .execute(&*state.db_pool)
-        .await?;
+        .execute(&state.pool)
+        .await;
+    match result {
+        Ok(_) => Ok(()),
+        Err(_) => Err(CustomError::GenericError),
+    }
+}
 
-    // Return a success result
-    Ok(CommandResult { success: true })
+#[tauri::command]
+pub async fn get_all_users(state: State<'_, PgPoolWrapper>) -> Result<Vec<User>, CustomError> {
+    let query = "SELECT * FROM users";
+
+    let result = sqlx::query_as::<_, User>(query)
+        .fetch_all(&state.pool)
+        .await;
+
+    match result {
+        Ok(users) => Ok(users),
+        Err(_) => Err(CustomError::GenericError),
+    }
+}
+
+#[tauri::command]
+pub async fn get_user(
+    id: i32,
+    state: State<'_, PgPoolWrapper>,
+) -> Result<Option<User>, CustomError> {
+    let query = "SELECT * FROM users WHERE id = $1";
+
+    let result = sqlx::query_as::<_, User>(query)
+        .bind(&id)
+        .fetch_optional(&state.pool)
+        .await;
+
+    match result {
+        Ok(user) => Ok(user),
+        Err(_) => Err(CustomError::GenericError),
+    }
+}
+
+#[tauri::command]
+pub async fn delete_user(
+    email: String,
+    state: State<'_, PgPoolWrapper>,
+) -> Result<(), CustomError> {
+    let query = "DELETE FROM users WHERE email = $1";
+
+    let result = sqlx::query(query).bind(&email).execute(&state.pool).await;
+
+    match result {
+        Ok(_) => Ok(()),
+        Err(_) => Err(CustomError::GenericError),
+    }
 }
--- src-tauri/src/routes/users.rs
+++ src-tauri/src/routes/users.rs
@@ -1,19 +1,17 @@
 use serde::{Deserialize, Serialize};
-use sqlx::FromRow;
-use sqlx::PgPool;
+use sqlx::{sqlite::SqlitePool, FromRow, PgPool};
 use tauri::State;
 use thiserror::Error;
-// #[derive(Serialize, Deserialize, Debug)]
-// struct Record {
-//     id: i64,
-//     project_name: String,
-// }
 
-pub struct PgPoolWrapper {
-    pub pool: PgPool,
+pub enum Database {
+    Postgres(PgPool),
+    SQLite(SqlitePool),
 }
 
-// #[derive(Serialize, Deserialize, Debug)]
+pub struct DatabasePool {
+    pub pool: Database,
+}
+
 #[derive(Serialize, Deserialize, Debug, FromRow)]
 pub struct User {
     pub name: String,
@@ -23,23 +21,10 @@ pub struct User {
     pub is_active: bool,
 }
 
-// impl User {
-//     pub fn new(name: String, role: i32, email: String, password: String, is_active: bool) -> Self {
-//         Self {
-//             name,
-//             role,
-//             email,
-//             password,
-//             is_active,
-//         }
-//     }
-// }
-
 #[derive(Error, Serialize, Debug)]
 pub enum CustomError {
     #[error("An error occurred")]
     GenericError,
-    // Add other error variants as needed
 }
 
 #[tauri::command]
@@ -48,91 +33,112 @@ pub async fn greet(name: String) -> String {
 }
 
 #[tauri::command]
-pub async fn create(user: User, state: State<'_, PgPoolWrapper>) -> Result<(), CustomError> {
-    let query =
-        "INSERT INTO users (name, role, email, password, is_active) VALUES ($1, $2, $3, $4, $5)";
-
-    let result = sqlx::query(query)
-        .bind(&user.name)
-        .bind(&user.role)
-        .bind(&user.email)
-        .bind(&user.password)
-        .bind(&user.is_active)
-        .execute(&state.pool)
-        .await;
-
-    match result {
-        Ok(_) => Ok(()),
-        Err(_) => Err(CustomError::GenericError),
+pub async fn create(user: User, state: State<'_, DatabasePool>) -> Result<(), CustomError> {
+    match &state.pool {
+        Database::Postgres(pool) => {
+            let query = "INSERT INTO users (name, role, email, password, is_active) VALUES ($1, $2, $3, $4, $5)";
+            let result = sqlx::query(query)
+                .bind(&user.name)
+                .bind(&user.role)
+                .bind(&user.email)
+                .bind(&user.password)
+                .bind(&user.is_active)
+                .execute(pool)
+                .await;
+            result.map_err(|_| CustomError::GenericError)?;
+        }
+        Database::SQLite(pool) => {
+            let query =
+                "INSERT INTO users (name, role, email, password, is_active) VALUES (?, ?, ?, ?, ?)";
+            let result = sqlx::query(query)
+                .bind(&user.name)
+                .bind(&user.role)
+                .bind(&user.email)
+                .bind(&user.password)
+                .bind(&user.is_active)
+                .execute(pool)
+                .await;
+            result.map_err(|_| CustomError::GenericError)?;
+        }
     }
+    Ok(())
 }
 
 #[tauri::command]
 pub async fn update(
     id: i32,
     user: User,
-    state: State<'_, PgPoolWrapper>,
+    state: State<'_, DatabasePool>,
 ) -> Result<(), CustomError> {
-    let query = "UPDATE users SET name = $1, role = $2, email = $3, password = $4, is_active = $5 WHERE id = $6";
-
-    let result = sqlx::query(query)
-        .bind(&user.name)
-        .bind(&user.role)
-        .bind(&user.email)
-        .bind(&user.password)
-        .bind(&user.is_active)
-        .bind(&id)
-        .execute(&state.pool)
-        .await;
-    match result {
-        Ok(_) => Ok(()),
-        Err(_) => Err(CustomError::GenericError),
+    match &state.pool {
+        Database::Postgres(pool) => {
+            let query = "UPDATE users SET name = $1, role = $2, email = $3, password = $4, is_active = $5 WHERE id = $6";
+            let result = sqlx::query(query)
+                .bind(&user.name)
+                .bind(&user.role)
+                .bind(&user.email)
+                .bind(&user.password)
+                .bind(&user.is_active)
+                .bind(&id)
+                .execute(pool)
+                .await;
+            result.map_err(|_| CustomError::GenericError)?;
+        }
+        Database::SQLite(pool) => {
+            // SQLite specific code for update
+        }
     }
+    Ok(())
 }
 
 #[tauri::command]
-pub async fn get_all_users(state: State<'_, PgPoolWrapper>) -> Result<Vec<User>, CustomError> {
-    let query = "SELECT * FROM users";
-
-    let result = sqlx::query_as::<_, User>(query)
-        .fetch_all(&state.pool)
-        .await;
-
-    match result {
-        Ok(users) => Ok(users),
-        Err(_) => Err(CustomError::GenericError),
+pub async fn get_all_users(state: State<'_, DatabasePool>) -> Result<Vec<User>, CustomError> {
+    match &state.pool {
+        Database::Postgres(pool) => {
+            let query = "SELECT * FROM users";
+            let result = sqlx::query_as::<_, User>(query).fetch_all(pool).await;
+            result.map_err(|_| CustomError::GenericError)
+        }
+        Database::SQLite(pool) => {
+            // SQLite specific code for fetching all users
+            unimplemented!("SQLite specific code for fetching all users")
+        }
     }
 }
 
 #[tauri::command]
 pub async fn get_user(
     id: i32,
-    state: State<'_, PgPoolWrapper>,
+    state: State<'_, DatabasePool>,
 ) -> Result<Option<User>, CustomError> {
-    let query = "SELECT * FROM users WHERE id = $1";
-
-    let result = sqlx::query_as::<_, User>(query)
-        .bind(&id)
-        .fetch_optional(&state.pool)
-        .await;
-
-    match result {
-        Ok(user) => Ok(user),
-        Err(_) => Err(CustomError::GenericError),
+    match &state.pool {
+        Database::Postgres(pool) => {
+            let query = "SELECT * FROM users WHERE id = $1";
+            let result = sqlx::query_as::<_, User>(query)
+                .bind(&id)
+                .fetch_optional(pool)
+                .await;
+            result.map_err(|_| CustomError::GenericError)
+        }
+        Database::SQLite(pool) => {
+            // SQLite specific code for fetching a user
+            unimplemented!("SQLite specific code for fetching a user")
+        }
     }
 }
 
 #[tauri::command]
-pub async fn delete_user(
-    email: String,
-    state: State<'_, PgPoolWrapper>,
-) -> Result<(), CustomError> {
-    let query = "DELETE FROM users WHERE email = $1";
-
-    let result = sqlx::query(query).bind(&email).execute(&state.pool).await;
-
-    match result {
-        Ok(_) => Ok(()),
-        Err(_) => Err(CustomError::GenericError),
+pub async fn delete_user(email: String, state: State<'_, DatabasePool>) -> Result<(), CustomError> {
+    match &state.pool {
+        Database::Postgres(pool) => {
+            let query = "DELETE FROM users WHERE email = $1";
+            let result = sqlx::query(query).bind(&email).execute(pool).await;
+            result.map_err(|_| CustomError::GenericError)?;
+        }
+        Database::SQLite(pool) => {
+            // SQLite specific code for deleting a user
+            unimplemented!("SQLite specific code for deleting a user")
+        }
     }
+    Ok(())
 }
